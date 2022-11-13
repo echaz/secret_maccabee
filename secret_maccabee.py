98cbac22fe4de42a47e91d708b7c6d667497c5c7
echaz <eric.chazan@gmail.com>
2022-11-13 15:52:15 -0500

    command line arguments, Google spreadsheet integration, and objects are better than lists

--- secret_maccabee.py
+++ secret_maccabee.py
@@ -1,30 +1,49 @@
+from argparse import ArgumentParser
 from copy import copy
-from traceback import print_exc
+from csv import reader
+from io import StringIO
 from os import getenv
 from random import shuffle
-import smtplib
-from invitee_groups import groups
+from smtplib import SMTP_SSL
+from traceback import print_exc
+
+from googleapiclient.discovery import build
+from google.oauth2.service_account import Credentials
+
 
 # If this is family looking at this script, "oh hai."
 # If you found this while you were interviewing me, this is just for kicks
 
 # I dont have time to send personalized emails, plus I would screw it up
-gmail_user = getenv("GMAILUSER")
-gmail_password = getenv("GMAILPASSWORD")
+# TODO: I think I should be able to use the oauth creds to send email
+gmail_user = getenv("GOOGLE_USER")
+gmail_password = getenv("GOOGLE_PASSWORD")
+google_sheet_id = getenv("GOOGLE_SHEET_ID")
+google_oauth_file = getenv("GOOGLE_OAUTH_CREDS")
 
-def send_email(to_email, secret_maccabee_person, physical_address=None):
+class Participant:
+    def __init__(self, name, email, in_person, physical_address):
+        self.name = name
+        self.email = email
+        self.in_person = in_person
+        self.physical_address = physical_address
+
+    def __repr__(self):
+        return self.name
+
+def send_email(to_email, secret_maccabee_person):
     from_line = f"From: {gmail_user}"
     to_line = f"To: {to_email}"
     subject_line = 'Subject: Your Secret Maccabee information'
     blank_line = '\n'
-    body = f"Your Secret Macabee is: {secret_maccabee_person}"
-    if physical_address:
-        body += f"\nThis year they are not in person, their mailing address is:\n{physical_address}"
+    body = f"Your Secret Macabee is: {secret_maccabee_person.name}"
+    if not secret_maccabee_person.in_person:
+        body += f"\nThis year they are not in person, their mailing address is:\n{secret_maccabee_person.address}"
 
     email_text = '\n'.join([from_line, to_line, subject_line, blank_line, body])
 
     try:
-        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server = SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(gmail_user, gmail_password)
         server.sendmail(gmail_user, to_email, email_text)
@@ -35,19 +54,46 @@ def send_email(to_email, secret_maccabee_person, physical_address=None):
         print ('oh noes!')
         print_exc()
 
-# draw names for a secret santa, or the jewish variant "secret maccabee".
-# TODO: gift recommendation
-def validate_input():
-    # input validation
-    assert len(groups) == 2
-    for group in groups:
-        for person in group:
-            assert len(person) == 3  # name, email, remote boolean
-        for value in group:
-            # consider these todos:
-            assert value != "UNKNOWN"
+def download_sheet_as_csv(drive_file_id=google_sheet_id, creds=google_oauth_file):
+    ''' download the google spreadsheet as a csv, and return the csv_string '''
+    credentials = Credentials.from_service_account_file(creds)
+    drive_service = build('drive', 'v3', credentials=credentials)
+    csv_string = drive_service.files().export_media(fileId=drive_file_id, mimeType='text/csv').execute()
+    return csv_string.decode('utf-8')
 
-    return shuffle_groups(groups[0], groups[1])
+
+def parse_input():
+    csv_string = download_sheet_as_csv()
+
+    buff = StringIO(csv_string)
+
+    csv_reader = reader(buff)
+    headers = next(csv_reader)
+    header_row = ['Name', 'email address', 'Will attend this year', 'Participating', 'Bender?', 'Address']
+
+    # header validation:
+    for index, col in enumerate(headers):
+        assert col.strip().lower() == header_row[index].strip().lower()
+
+    print('the file seems valid')
+
+    group_1_list = []
+    group_2_list = []
+    for row in csv_reader:
+        participating = row[3]
+
+        if participating.lower() == "yes":
+            # name, email, in_person, physical_address
+            person = Participant(row[0], row[1], row[2], row[5])
+            group_val = row[4]
+            group_1 = group_val.strip().lower() == 'true'
+
+            if group_1:
+                group_1_list.append(person)
+            else:
+                group_2_list.append(person)
+
+    return group_1_list, group_2_list
 
 
 def shuffle_groups(group_1, group_2):
@@ -67,7 +113,7 @@ def shuffle_groups(group_1, group_2):
         recipients.extend(group_1)
 
         for person_index in range(len(gifters)):
-            if gifters[person_index][0] == recipients[person_index][0]:
+            if gifters[person_index].name == recipients[person_index].name:
                 continue
             valid = True
             break
@@ -78,10 +124,20 @@ def shuffle_groups(group_1, group_2):
 # since this guy is going to send emails, I'm going to not call main until I either have solid tests or have run this for a few years.
 # consider this an example of how I'm calling this from ipython
 if __name__ == '__main__':
-    gifters, recipients = validate_input()
+    parser = ArgumentParser()
+    parser.add_argument("--dry-run", type=bool, required=False, default=True,
+                        help="build a little, test a little, learn alot")
+    parser.add_argument("--pause", type=bool, required=False, default=True,
+                        help='human in the loop incase mistakes are made')
+    gifters, recipients = parse_input()
 
-    # TODO: I think zip or something works better here
-    for person_index in range(len(gifters)):
-        print (f"# {gifters[person_index][0]} gifts to {recipients[person_index][0]}")
-        print(f"send_email('{gifters[person_index][1]}','{recipients[person_index][0]}','{recipients[person_index][2]}')")
+    args = parser.parse_args()
+    groups = parse_input()
+    gifters, recipients = shuffle_groups(*groups)
 
+    if args.dry_run:
+        for person_index in range(len(gifters)):
+            print (f"# {gifters[person_index].name} gifts to {recipients[person_index]}")
+            print(f"send_email('{gifters[person_index].email}','{recipients[person_index]}')")
+    else:
+        raise Exception(f"pause is {args.pause}")
